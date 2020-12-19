42529166439d4b1ea96cae5f0d85f8a57f3f837a
cobaltcore <cobaltcore@yandex.com>
2020-12-19 02:16:13 +0100

    Return to hourly scraping

--- app/main.py
+++ app/main.py
@@ -307,11 +307,10 @@ class Token(BaseModel):
 
 
 @app.on_event("startup")
-@repeat_every(seconds=600)  # 10 minutes
+@repeat_every(seconds=60 * 60)  # 1 hour
 async def trigger_crawl_tfgs():
     print("Running scheduled crawl task")
     await crawl_tfgs()
-
 
 
 @app.on_event("startup")
